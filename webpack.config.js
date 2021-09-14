4ca4691421fb0b622e5f9ed946e9e5f44b22d67c
toshi <90437814+toshisai@users.noreply.github.com>
2021-09-14 13:36:23 +0900

    ES6, React, Vue, etc

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
     entry: './src/javascripts/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
@@ -12,18 +15,48 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.vue/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'vue-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', { 'targets': '> 25%, not dead' }],
+                                '@babel/preset-react'
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(css|sass|scss)/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader
                     },
                     {
-                        loader: 'css-loader'
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: false
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
                     }
                 ]
             },
             {
-                test: /\.(png|jpg)/,
+                test: /\.(png|jpg|jpeg)/,
                 type: 'asset/resource',
                 generator: {
                     filename: 'images/[name][ext]'
@@ -35,7 +68,17 @@ module.exports = {
                     //         esModule: false,
                     //         name: 'images/[name].[ext]'
                     //     }
-                    // }
+                    // },
+
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            }
+                        }
+                    }
                 ]
             },
             {
@@ -55,6 +98,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
             filename: './stylesheets/main.css'
         }),
